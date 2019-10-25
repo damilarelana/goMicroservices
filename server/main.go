ac19f1d67294061fc856f52c5903791e04aec4ae
Lana <Damilare Lana>
2019-10-25 02:23:23 +0100

    added the service handlers for Average, Min, Max, Sum, Sort

--- server/main.go
+++ server/main.go
@@ -27,12 +27,42 @@ func (server *mathService) Add(ctx context.Context, r *ms.AddRequest) (*ms.AddRe
 	return &ms.AddResponse{Addition: mf.Add(r.X, r.Y)}, nil
 }
 
+// Average service handler method
+func (server *mathService) Average(ctx context.Context, r *ms.AverageRequest) (*ms.AverageResponse, error) {
+	log.Printf("Received input array %v", r.Array)
+	return &ms.AverageResponse{Average: mf.Average(r.Array)}, nil
+}
+
+// Max service handler method
+func (server *mathService) Max(ctx context.Context, r *ms.MaxRequest) (*ms.MaxResponse, error) {
+	log.Printf("Received input array %v", r.Array)
+	return &ms.MaxResponse{Maximum: mf.Max(r.Array)}, nil
+}
+
+// Min service handler method
+func (server *mathService) Min(ctx context.Context, r *ms.MinRequest) (*ms.MinResponse, error) {
+	log.Printf("Received input array %v", r.Array)
+	return &ms.MinResponse{Minimum: mf.Min(r.Array)}, nil
+}
+
+// Sort service handler method
+func (server *mathService) Sort(ctx context.Context, r *ms.SortRequest) (*ms.SortResponse, error) {
+	log.Printf("Received input array %v", r.Array)
+	return &ms.SortResponse{SortedArray: mf.Bubblesort(r.Array)}, nil
+}
+
+// Sum service handler method
+func (server *mathService) Sum(ctx context.Context, r *ms.SumRequest) (*ms.SumResponse, error) {
+	log.Printf("Received input array %v", r.Array)
+	return &ms.SumResponse{ArrayValuesSum: mf.Sum(r.Array)}, nil
+}
+
 func main() {
 
 	// start a server listener
 	listener, err := net.Listen("tcp", port)
 	if err != nil {
-		log.Fatal(errors.Wrap(err, fmt.Sprintf("Failed to listen on port %v due to %v", port)))
+		log.Fatal(errors.Wrap(err, fmt.Sprintf("Failed to listen on port %v", port)))
 	}
 
 	// instanstiate a gRPC server
