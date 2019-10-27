a009ed5225fd447b22f7769c72471f33c56edda7
Lana <Damilare Lana>
2019-10-27 04:18:12 +0100

    added averageHandler(), sortHandler(), minHandler(), maxHandler(), sumHandler()

--- api/main.go
+++ api/main.go
@@ -59,25 +59,103 @@ func strToArray(str string) (array []float64) {
 	return array
 }
 
-// averageHandler endpoint focus on the Average service that finds the average value of an Array element
+// sortHandler endpoint focus on the sum service that gives the summation of the Array elements
+func sortHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json: charset=UFT-8") // set the content header type
+	vars := mux.Vars(r)                                               // extract usable information from request object by parsing the inputs x and y
+	inputArray := strToArray(vars["array"])                           // extract value of array from the variable request arguments
+	ctx, cancel := context.WithTimeout(context.TODO(), time.Minute)   // initialize resources context
+	defer cancel()
+	req := &ms.SortRequest{ // initialize the request struct
+		Array: inputArray,
+	}
+	g := gRPCClient() // call the initialized client
+	resp, err := g.Sort(ctx, req)
+	if err == nil {
+		msg := fmt.Sprintf("Sorted array is %f:", resp.SortedArray)
+		json.NewEncoder(w).Encode(msg)
+	} else {
+		log.Fatal(errors.Wrap(err, fmt.Sprintf("Internal Server error: sortHandler")))
+	}
+}
+
+// sumHandler endpoint focus on the sum service that gives the summation of the Array elements
+func sumHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json: charset=UFT-8") // set the content header type
+	vars := mux.Vars(r)                                               // extract usable information from request object by parsing the inputs x and y
+	inputArray := strToArray(vars["array"])                           // extract value of array from the variable request arguments
+	ctx, cancel := context.WithTimeout(context.TODO(), time.Minute)   // initialize resources context
+	defer cancel()
+	req := &ms.SumRequest{ // initialize the request struct
+		Array: inputArray,
+	}
+	g := gRPCClient() // call the initialized client
+	resp, err := g.Sum(ctx, req)
+	if err == nil {
+		msg := fmt.Sprintf("Minimum element is %f:", resp.ArrayValuesSum)
+		json.NewEncoder(w).Encode(msg)
+	} else {
+		log.Fatal(errors.Wrap(err, fmt.Sprintf("Internal Server error: sumHandler")))
+	}
+}
+
+// minHandler endpoint focus on the Min service that finds the minimum value amongst Array elements
+func minHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json: charset=UFT-8") // set the content header type
+	vars := mux.Vars(r)                                               // extract usable information from request object by parsing the inputs x and y
+	inputArray := strToArray(vars["array"])                           // extract value of array from the variable request arguments
+	ctx, cancel := context.WithTimeout(context.TODO(), time.Minute)   // initialize resources context
+	defer cancel()
+	req := &ms.MinRequest{ // initialize the request struct
+		Array: inputArray,
+	}
+	g := gRPCClient() // call the initialized client
+	resp, err := g.Min(ctx, req)
+	if err == nil {
+		msg := fmt.Sprintf("Minimum element is %f:", resp.Minimum)
+		json.NewEncoder(w).Encode(msg)
+	} else {
+		log.Fatal(errors.Wrap(err, fmt.Sprintf("Internal Server error: minHandler")))
+	}
+}
+
+// maxHandler endpoint focus on the Max service that finds the maximum value amongst Array elements
+func maxHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json: charset=UFT-8") // set the content header type
+	vars := mux.Vars(r)                                               // extract usable information from request object by parsing the inputs x and y
+	inputArray := strToArray(vars["array"])                           // extract value of array from the variable request arguments
+	ctx, cancel := context.WithTimeout(context.TODO(), time.Minute)   // initialize resources context
+	defer cancel()
+	req := &ms.MaxRequest{ // initialize the request struct
+		Array: inputArray,
+	}
+	g := gRPCClient() // call the initialized client
+	resp, err := g.Max(ctx, req)
+	if err == nil {
+		msg := fmt.Sprintf("Maximum element is %f:", resp.Maximum)
+		json.NewEncoder(w).Encode(msg)
+	} else {
+		log.Fatal(errors.Wrap(err, fmt.Sprintf("Internal Server error: maxHandler")))
+	}
+}
+
+// averageHandler endpoint focus on the Average service that finds the average value amongst Array elements
 func averageHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json: charset=UFT-8") // set the content header type
 	vars := mux.Vars(r)                                               // extract usable information from request object by parsing the inputs x and y
-	x := strToInt(vars["x"])                                          // extract value of x from the variable request arguments
-	y := strToInt(vars["y"])                                          // extract value of y from the variale request arguments
+	inputArray := strToArray(vars["array"])                           // extract value of array from the variable request arguments
 	ctx, cancel := context.WithTimeout(context.TODO(), time.Minute)   // initialize resources context
 	defer cancel()
-	req := &ms.AddRequest{ // initialize the request struct
-		X: x,
-		Y: y,
+	req := &ms.AverageRequest{ // initialize the request struct
+		Array: inputArray,
 	}
 	g := gRPCClient() // call the initialized client
-	resp, err := g.Add(ctx, req)
+	resp, err := g.Average(ctx, req)
 	if err == nil {
-		msg := fmt.Sprintf("Addition is %d:", resp.Addition)
+		msg := fmt.Sprintf("Average is %f:", resp.Average)
 		json.NewEncoder(w).Encode(msg)
 	} else {
-		log.Fatal(errors.Wrap(err, fmt.Sprintf("Internal Server error")))
+		log.Fatal(errors.Wrap(err, fmt.Sprintf("Internal Server error: averageHandler")))
 	}
 }
 
@@ -99,7 +177,7 @@ func addHandler(w http.ResponseWriter, r *http.Request) {
 		msg := fmt.Sprintf("Addition is %d:", resp.Addition)
 		json.NewEncoder(w).Encode(msg)
 	} else {
-		log.Fatal(errors.Wrap(err, fmt.Sprintf("Internal Server error")))
+		log.Fatal(errors.Wrap(err, fmt.Sprintf("Internal Server error: addHandler")))
 	}
 }
 
@@ -110,6 +188,10 @@ func serviceRequestHandlers() {
 	muxRouter.HandleFunc("/", msAPIHomePage)
 	muxRouter.HandleFunc("/{x}/{y}", addHandler).Methods("GET")     // the add service endpoint mapping
 	muxRouter.HandleFunc("/{array}", averageHandler).Methods("GET") // the average service endpoint mapping
+	muxRouter.HandleFunc("/{array}", maxHandler).Methods("GET")     // the maximum service endpoint mapping
+	muxRouter.HandleFunc("/{array}", minHandler).Methods("GET")     // the minimum service endpoint mapping
+	muxRouter.HandleFunc("/{array}", sumHandler).Methods("GET")     // the summation service endpoint mapping
+	muxRouter.HandleFunc("/{array}", sortHandler).Methods("GET")    // the sorting service endpoint mapping
 	fmt.Println("API is up an running now at : 8080")
 	log.Fatal(http.ListenAndServe(":8080", muxRouter)) // set the port where the http server listens and serves the API from
 }
